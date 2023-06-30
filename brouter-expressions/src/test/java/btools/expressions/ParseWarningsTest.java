7fc3c451ac40b6eddbb576b9e0e20d7b1362afcb
radim-asamm <radim.vecerek@asamm.com>
2023-06-30 22:23:43 +0200

    Just make a test more readable

--- brouter-expressions/src/test/java/btools/expressions/ParseWarningsTest.java
+++ brouter-expressions/src/test/java/btools/expressions/ParseWarningsTest.java
@@ -3,27 +3,55 @@ package btools.expressions;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 public class ParseWarningsTest {
 
+  private static class TestPair {
+    String sample;
+    int expectedResultSize;
+
+    public TestPair(String sample, int expectedResultSize) {
+      this.sample = sample;
+      this.expectedResultSize = expectedResultSize;
+    }
+  }
+
   @Test
   public void testParsedSamples() {
+
     // Expected syntax:
     // ---context:way
     // assign warnings = identifier1&identifier2
     // ...
     // ---context:node
     // assign warnings = identifier1&identifier2&identifier3
-    String[] samples = new String[]{null, "", " ", "   ", " w", " w ", "w1&w2", " w1&w2 ", " w1&w1 ", "&&&", " &", "& & &  ", "&w1&", "w1,w2", "walk-hike.difficult.very"};
-    int[] expectedResultsSizes = new int[]{0, 0, 0, 0, 1, 1, 2, 2, 1, 0, 0, 0, 1, 1, 1};
-    Assert.assertEquals("Given resources size mismatch", samples.length, expectedResultsSizes.length);
-    for (int i = 0; i < samples.length; i++) {
+
+    List<TestPair> pairs = new ArrayList<>();
+    pairs.add(new TestPair(null, 0));
+    pairs.add(new TestPair("", 0));
+    pairs.add(new TestPair(" ", 0));
+    pairs.add(new TestPair("      ", 0));
+    pairs.add(new TestPair("      w", 1));
+    pairs.add(new TestPair("      w  ", 1));
+    pairs.add(new TestPair("w1&w2", 2));
+    pairs.add(new TestPair(" w1&w2  ", 2));
+    pairs.add(new TestPair(" w1&w1 ", 1));
+    pairs.add(new TestPair("&&&", 0));
+    pairs.add(new TestPair(" &", 0));
+    pairs.add(new TestPair("& & &  ", 0));
+    pairs.add(new TestPair("&w1&", 1));
+    pairs.add(new TestPair("w1,w2", 1));
+    pairs.add(new TestPair("walk-hike.difficult.very", 1));
+
+    for (TestPair tp : pairs) {
       System.out.println();
-      System.out.println(">" + samples[i] + "<");
-      Set<String> parsed = new BExpression(samples[i]).parseWarnings();
+      System.out.println(">" + tp.sample + "<");
+      Set<String> parsed = new BExpression(tp.sample).parseWarnings();
       System.out.println(parsed);
-      Assert.assertEquals(parsed.size(), expectedResultsSizes[i]);
+      Assert.assertEquals(parsed.size(), tp.expectedResultSize);
     }
   }
 }
